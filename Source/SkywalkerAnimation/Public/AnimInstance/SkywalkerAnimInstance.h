6884d2040b6a216c2b0f6201e918826f67256234
Shyfan <410713847@qq.com>
2024-10-24 18:35:25 +0800

    保存

---
+++ Source/SkywalkerAnimation/Public/AnimInstance/SkywalkerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SkywalkerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERANIMATION_API USkywalkerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
